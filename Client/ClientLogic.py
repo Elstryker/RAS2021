488ce7e2044462e0f6319c896d7d70aad46fb533
Elstryker <rubencorreiacerqueira@gmail.com>
2022-01-18 18:51:43 +0000

    If not log in print stuff

--- Client/ClientLogic.py
+++ Client/ClientLogic.py
@@ -42,7 +42,7 @@ class ClientLogic:
 
         data = message.encode('utf-8')
         self.sock.send(data)
-        data = self.sock.recv(2048)
+        data = self.sock.recv(10000)
         response = json.loads(data.decode('utf-8'))
 
         # Retrieve additional info
@@ -102,7 +102,6 @@ class ClientLogic:
         args = [option,"GET",eventID]
         response = self.requestServer(args)
 
-        #TODO: reagir diferente dependendo da resposta
         print(response["Message"])
         if not response["Success"]:
             return
@@ -145,19 +144,12 @@ class ClientLogic:
         response = self.requestServer(args)
         aposta = self.client_gui.show_betslip(response, 6)
 
-
-           
-
-            
-
         args = [option,aposta]
         response = self.requestServer(args)
 
         print(response)
 
     def cancelBetSlip(self,option):
-        args = [option]
-        response = self.requestServer(args)
 
         if not self.clientInfo.loggedIn:
             args = ["P"]
@@ -165,6 +157,9 @@ class ClientLogic:
             self.clientInfo.cancelBetSlipNotLoggedIn()
 
             return
+
+        args = [option]
+        response = self.requestServer(args)
 
         print(response["Message"])
 
@@ -210,31 +205,37 @@ class ClientLogic:
         currency = ""
         amount = ""
 
-        while not currency.isdigit() or int(currency) < 0 or int(currency) > 100 or int(currency) >= len(self.clientInfo.availableCurrencies):
-            currency = self.client_gui.pede_moeda(self.clientInfo.loggedIn, self.clientInfo.getEvents(), self.clientInfo.availableCurrencies,  False, self.clientInfo.getPages())
+        if self.clientInfo.loggedIn:
+            while not currency.isdigit() or int(currency) < 0 or int(currency) > 100 or int(currency) >= len(self.clientInfo.availableCurrencies):
+                currency = self.client_gui.pede_moeda(self.clientInfo.loggedIn, self.clientInfo.getEvents(), self.clientInfo.availableCurrencies,  False, self.clientInfo.getPages())
 
-        while not amount.isdigit() or int(amount) < 1:    
-            amount = self.client_gui.ask_info(self.clientInfo.loggedIn, self.clientInfo.getEvents(), 3, self.clientInfo.getPages())
-        
-        args = [option,self.clientInfo.availableCurrencies[int(currency)],amount]
-        response = self.requestServer(args)
+            while not amount.isdigit() or int(amount) < 1:    
+                amount = self.client_gui.ask_info(self.clientInfo.loggedIn, self.clientInfo.getEvents(), 3, self.clientInfo.getPages())
+            
+            args = [option,self.clientInfo.availableCurrencies[int(currency)],amount]
+            response = self.requestServer(args)
+            print(response["Message"])
+        else:
+            self.client_gui.invalid_info(self.clientInfo.loggedIn, 1)
 
-        print(response["Message"])
 
 
     def withdrawMoney(self,option):
         currency = ""
         amount = ""
 
-        while not currency.isdigit() or int(currency) < 0 or int(currency) >= len(self.clientInfo.availableCurrencies):
-            currency = self.client_gui.pede_moeda(self.clientInfo.loggedIn, self.clientInfo.getEvents(), self.clientInfo.availableCurrencies, False, self.clientInfo.getPages())
-        
-        while not amount.isdigit() or int(amount) < 1:    
-            amount = self.client_gui.ask_info(self.clientInfo.loggedIn, self.clientInfo.getEvents(), 3, self.clientInfo.getPages())
-        
-        args = [option,self.clientInfo.availableCurrencies[int(currency)],amount]
-        mensagem = self.requestServer(args)
-        print(mensagem["Message"])
+        if self.clientInfo.loggedIn:
+            while not currency.isdigit() or int(currency) < 0 or int(currency) >= len(self.clientInfo.availableCurrencies):
+                currency = self.client_gui.pede_moeda(self.clientInfo.loggedIn, self.clientInfo.getEvents(), self.clientInfo.availableCurrencies, False, self.clientInfo.getPages())
+            
+            while not amount.isdigit() or int(amount) < 1:    
+                amount = self.client_gui.ask_info(self.clientInfo.loggedIn, self.clientInfo.getEvents(), 3, self.clientInfo.getPages())
+            
+            args = [option,self.clientInfo.availableCurrencies[int(currency)],amount]
+            mensagem = self.requestServer(args)
+            print(mensagem["Message"])
+        else:
+            self.client_gui.invalid_info(self.clientInfo.loggedIn, 1)
 
     def changePage(self,option):
         self.clientInfo.previousPage() if option == "A" else self.clientInfo.nextPage()
@@ -248,21 +249,20 @@ class ClientLogic:
         args = [option]
         response = self.requestServer(args)
         
-        if len(response["History"]) == 0:
-            self.client_gui.invalid_info(self.clientInfo.loggedIn, 3)
+        if self.clientInfo.loggedIn:
+            if len(response["History"]) == 0:
+                self.client_gui.invalid_info(self.clientInfo.loggedIn, 3)
+            else:
+                escolha = "a"
+
+                while not escolha.isdigit() or int(escolha) < 0:
+                    escolha = self.client_gui.show_history(self.clientInfo.loggedIn, response["History"])
+
+                boletim = self.encontra_boletim(response["History"], int(escolha))
+
+                self.client_gui.show_betslip(self.clientInfo.loggedIn, {"BetSlip":boletim}, 5)
         else:
-            escolha = "a"
-
-            while not escolha.isdigit() or int(escolha) < 0:
-                escolha = self.client_gui.show_history(self.clientInfo.loggedIn, response["History"])
-
-            boletim = self.encontra_boletim(response["History"], int(escolha))
-
-            self.client_gui.show_betslip(self.clientInfo.loggedIn, {"BetSlip":boletim}, 5)
-
-        # Falta GUI e decisões
-    def filtrar (self, ):
-        pass
+            self.client_gui.invalid_info(self.clientInfo.loggedIn, 1)
 
 
     def encontra_boletim(self, boletins : list, escolha : int):
@@ -276,15 +276,19 @@ class ClientLogic:
         return resposta
 
     def exchangeCurrency(self,option):
-        fromCurrency = self.client_gui.pede_moeda(self.clientInfo.loggedIn, self.clientInfo.getEvents(), self.clientInfo.availableCurrencies, False, self.clientInfo.getPages(), 1)
-        toCurrency = self.client_gui.pede_moeda(self.clientInfo.loggedIn, self.clientInfo.getEvents(), self.clientInfo.availableCurrencies, False, self.clientInfo.getPages(), 2)
+        
+        if self.clientInfo.loggedIn:
+            fromCurrency = self.client_gui.pede_moeda(self.clientInfo.loggedIn, self.clientInfo.getEvents(), self.clientInfo.availableCurrencies, False, self.clientInfo.getPages(), 1)
+            toCurrency = self.client_gui.pede_moeda(self.clientInfo.loggedIn, self.clientInfo.getEvents(), self.clientInfo.availableCurrencies, False, self.clientInfo.getPages(), 2)
 
-        amount = self.client_gui.ask_info(self.clientInfo.loggedIn, self.clientInfo.getEvents(), 3, self.clientInfo.getPages())
+            amount = self.client_gui.ask_info(self.clientInfo.loggedIn, self.clientInfo.getEvents(), 3, self.clientInfo.getPages())
 
-        args = [option,self.clientInfo.availableCurrencies[int(fromCurrency)],self.clientInfo.availableCurrencies[int(toCurrency)],amount]
-        response = self.requestServer(args)
+            args = [option,self.clientInfo.availableCurrencies[int(fromCurrency)],self.clientInfo.availableCurrencies[int(toCurrency)],amount]
+            response = self.requestServer(args)
 
-        #print(response["Message"])
+            #print(response["Message"])
+        else:
+            self.client_gui.invalid_info(self.clientInfo.loggedIn, 1)
     
     
     def login(self,option):
@@ -304,10 +308,13 @@ class ClientLogic:
         # print(response['Message'])
 
     def logout(self,option):
-        args = [option]
-        self.requestServer(args)
-        self.clientInfo.loggedIn = False
-        self.client_gui.username = None
+        if self.clientInfo.loggedIn:
+            args = [option]
+            self.requestServer(args)
+            self.clientInfo.loggedIn = False
+            self.client_gui.username = None
+        else:
+            self.client_gui.invalid_info(self.clientInfo.loggedIn, 1)
         
     def register(self,option):
         username = self.client_gui.ask_info(self.clientInfo.loggedIn, self.clientInfo.getEvents(), 0, self.clientInfo.getPages())
